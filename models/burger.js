2c1fca1c24146824b6b186aae1843f22b1661496
kylest0kes <kylestokes1551@gmail.com>
2020-06-22 22:06:05 -0400

    created the selectAll and updateOne functons in the burgers model

--- models/burger.js
+++ models/burger.js
@@ -1,5 +1,21 @@
 //import orm into this
-
+const orm = require("../config/orm.js");
 //write code
+const burger = {
+    selectAll: function(cb) {
+        orm.selectAll("burgers", function(res) {
+          cb(res);
+        });
+    },
+    insertOne: function() {
+
+    },
+    updateOne: function(objColVals, condition, cb) {
+        orm.updateOne("burgers", objColVals, condition, function(res) {
+            cb(res);
+        })
+    }
+}
 
 //export
+module.exports = burger;
