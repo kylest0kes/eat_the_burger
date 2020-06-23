9d2327494a0b0c811b9eae884b78cbf6b544a409
kylest0kes <kylestokes1551@gmail.com>
2020-06-22 22:08:52 -0400

    began pseudo coding burgers_controller.js

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,6 +1,10 @@
 const express = require("express");
+
+const router = express.Router();
+
 const burger = require("./models/burger.js");
 
 //Create Router file
 
 //export file
+module.exports = router;
